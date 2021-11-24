c70588c4930370a23fb4b6ed25c08f8160eec7b0
Wojciech Kryg <krygwojciech@gmail.com>
2021-11-24 16:46:30 +0100

    Change app colors

--- app/src/main/java/com/wojdor/fourthwallrecruitmenttask/ui/theme/Color.kt
+++ app/src/main/java/com/wojdor/fourthwallrecruitmenttask/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.wojdor.fourthwallrecruitmenttask.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val LightGreen200 = Color(0xFFC5E1A5)
+val LightGreen500 = Color(0xFF8BC34A)
+val LightGreen700 = Color(0xFF689F38)
+val Orange200 = Color(0xFFFFCC80)
+val DarkGrey = Color(0xFF121212)
